d2a7f81d3cb69b54759b9cb6db8d3ea4fe92d3c3
Havard <havard@1x-193-157-192-222.uio.no>
2016-03-30 14:09:14 +0200

    Added datascience

--- .dockercommands.sh
+++ .dockercommands.sh
@@ -8,14 +8,19 @@ alias dockerstart="docker-machine start default; docker-machine env; eval $(dock
 alias dockereval="eval $(docker-machine env)"
 dockereval
 
-# Start jupyter/datascience-notebook
+dataBash() {
+	docker run -v $(pwd):/home/jovyan/work --rm -it datascience /bin/bash
+}
+
+# Start datascience image
 datascience() {
 	if [ -z "$1" ]
 	then 
 		echo "need output port, e.g. 8888"
 	else
+		open http://$(docker-machine ip):$1
 		path=$(pwd)
-		docker run -v $path:/home/jovyan/work -p $1:8888 --rm jupyter/datascience-notebook
+		docker run -v $path:/home/jovyan/work -p $1:8888 --rm datascience
 	fi
 }
 
@@ -32,12 +37,11 @@ jupyterDark() {
 
 # Run pdflatex from container
 dpdflatex() {
-	path=$(pwd)
 	if [ -z "$1" ]
 	then	
 		echo "need file"
 	else
-		docker run --rm -v $path:/home/havakv/mnt -it latex /bin/bash -c "pdflatex $1"
+		docker run --rm -v $(pwd):/home/havakv/mnt -it latex /bin/bash -c "pdflatex $1"
 	fi
 }
 
